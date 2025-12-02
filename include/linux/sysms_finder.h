813134814e0326324ac5ed86873a4d29ab21465f
brokestar233 <3765589194@qq.com>
2025-12-02 21:50:01 +0800

    sysms_finder: cleanup unused symbols
    Change-Id: I18f437391dd82f9037aeb338c127263fec814586
    Signed-off-by: brokestar233 <3765589194@qq.com>

--- include/linux/sysms_finder.h
+++ include/linux/sysms_finder.h
@@ -2,6 +2,27 @@
 #ifndef _LINUX_GAME_PID_H
 #define _LINUX_GAME_PID_H
 
+
+enum {
+    SYMBOL_GAME_PID,
+	NR_SYMBOLS,
+};
+
+struct symbol_entry {
+	const char *name;
+	unsigned long addr;
+	bool found;
+};
+
+static struct symbol_entry symbols_status[NR_SYMBOLS] = {
+    [SYMBOL_GAME_PID] = {
+		.name = "game_pid",
+		.addr = 0,
+		.found = false,
+	},
+};
+
+unsigned long lookup_symbol(int symbol_index);
 bool check_game_pid(void);
 
 #endif /* _LINUX_GAME_PID_H */
