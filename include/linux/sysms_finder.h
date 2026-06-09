433ed40bf64c03120d5d431ca3037bd22c572011
Oscar <osm1019@gmail.com>
2026-06-08 20:21:21 -0700

    sysms_finder: fix redefinition errors by moving definitions out of header
    Change-Id: Iceb71c38a5f0160bd36995cde1fc52f5f75b8b0e

--- include/linux/sysms_finder.h
+++ include/linux/sysms_finder.h
@@ -2,9 +2,8 @@
 #ifndef _LINUX_GAME_PID_H
 #define _LINUX_GAME_PID_H
 
-
 enum {
-    SYMBOL_GAME_PID,
+	SYMBOL_GAME_PID,
 	NR_SYMBOLS,
 };
 
@@ -12,14 +11,6 @@ struct symbol_entry {
 	const char *name;
 	unsigned long addr;
 	bool found;
-};
-
-static struct symbol_entry symbols_status[NR_SYMBOLS] = {
-    [SYMBOL_GAME_PID] = {
-		.name = "game_pid",
-		.addr = 0,
-		.found = false,
-	},
 };
 
 unsigned long lookup_symbol(int symbol_index);
